d9a13f11e60ce38c749e109ea337757e9dded55a
SnakebirdG <samonester@gmail.com>
2020-03-28 14:01:49 +0100

    start

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # cajt
+
+Setup
+
+```
+python3 -m venv venv
+source venv/bin/activate
+pip3 install flask
+pip3 freeze > requirements.txt
+deactivate
+```
+
+```
+touch now.json index.py
+```
---
+++ index.py
@@ -0,0 +1,11 @@
+from flask import Flask, Response
+app = Flask(__name__)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask on ZEIT Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
