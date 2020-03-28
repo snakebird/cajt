a8d627cf0d090c30aa32e6bd11129902dedbff57
SnakebirdG <samonester@gmail.com>
2020-03-28 15:00:47 +0100

    json

--- index.py
+++ index.py
@@ -1,11 +1,27 @@
 from flask import Flask, Response
 app = Flask(__name__)
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response("<h1>Flask on ZEIT Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+    return app.make_response({'tasks': tasks})
+    # return Response("<h1>Flask on ZEIT Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
 
 if __name__ == '__main__':
     app.run(debug=True)
